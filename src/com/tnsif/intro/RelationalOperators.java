0e2aa881468762cc66da8cde477bd97f58113519
chikky_pathuri <pathurichikkyreddy@gmail.com>
2025-12-23 11:14:03 +0530

    Initial Java project – executed and verified

---
+++ src/com/tnsif/intro/RelationalOperators.java
@@ -0,0 +1,24 @@
+package com.tnsif.intro;
+
+public class RelationalOperators {
+
+	public static void main (String args[]) {
+		int num=26;
+		int num1=27;
+		System.out.println(num<num1);
+		System.out.println(num<=num1);
+		System.out.println(num>num1);
+		System.out.println(num>=num1);
+		System.out.println(num==num1);
+		System.out.println(num!=num1);
+		//using string
+		String name1="chikky";
+		String name2="sahana";
+		System.out.println(("stringcomparison:"+name1)==name2);
+		//using char
+		char surname1='p';
+		char surname2='j';
+		System.out.println("stringcomparison:"+ (surname1==surname2));
+	}
+
+}
