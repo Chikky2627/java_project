1d99e752e2026cd957c2c6f3eea260f024c3a410
chikky_pathuri <pathurichikkyreddy@gmail.com>
2026-01-06 21:14:41 +0530

    bbn

--- src/com/tnsif/intro/assignments/StringReverse.java
+++ src/com/tnsif/intro/assignments/StringReverse.java
@@ -3,8 +3,8 @@ package com.tnsif.intro.assignments;
 public class StringReverse {
    public static void main(String[] args) {
 	   String s="TEAM WORK";
-	   System.out.println("original String is:"+s);
-	   System.out.print("reversed string:");
+	   System.out.println("Original String is:"+s);
+	   System.out.print("Reversed String:");
 	   for(int i=s.length()-1;i>=0;i--) {
 		  System.out.print(s.charAt(i));
 	   }
